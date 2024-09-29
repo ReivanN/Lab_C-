b61b382fc43761580bb96892cd00fbb6f913a3c6
ReivanN <k20040806alexei@gmail.com>
2024-09-29 12:45:49 +0300

    Lab_02 commit

--- Board.hpp
+++ Board.hpp
@@ -3,4 +3,8 @@ class Board
   int width = 100;
   int height =100;
   
+  Board::Board(int width, int height) {}
+  int Board::GetWidth() const {}
+  int Board::GetHeight() const {}
+
 };
--- Lab_1.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
-struct Snake {
-    int x, y;
-    int tailX[100], tailY[100];
-    int nTail;
-    Direction dir;
-};
